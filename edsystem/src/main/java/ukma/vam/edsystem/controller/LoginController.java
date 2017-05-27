f6149a78d9d8785fe9f844df09588a47581bf146
Dyhdalo <sword6633@gmail.com>
2017-05-27 22:43:55 +0300

    add 403/404/500 pages
    messages

--- edsystem/src/main/java/ukma/vam/edsystem/controller/LoginController.java
+++ edsystem/src/main/java/ukma/vam/edsystem/controller/LoginController.java
@@ -31,4 +31,50 @@ public class LoginController {
 		return "index";
 	}
 	
+	/**
+	 * Deny page - if authenticated users go to page, which is designed for
+	 * another role.
+	 * 
+	 * @param RequestMapping
+	 *            URL of deny page
+	 * @return name of deny page
+	 * */
+	@RequestMapping(value = "/403", method = RequestMethod.GET)
+	public String accessDenied(Model model) {
+		model.addAttribute("title", "403");
+		model.addAttribute("head", "Ви не маєте доступу до цієї сторінки!");
+		model.addAttribute("img", "/edsystem/resources/img/403.gif");
+		return "message";
+	}
+	
+	/**
+	 * Error page - if users go to page, which doesn't exist.
+	 * 
+	 * @param RequestMapping
+	 *            URL of error page
+	 * @return name of error page
+	 * */
+	@RequestMapping(value = "/404", method = RequestMethod.GET)
+	public String error404(Model model) {
+		model.addAttribute("title", "404");
+		model.addAttribute("head", "Цієї сторінки не існує!");
+		model.addAttribute("img", "/edsystem/resources/img/404.gif");
+		return "message";
+	}
+	
+	/**
+	 * Error page - server error.
+	 * 
+	 * @param RequestMapping
+	 *            URL of error page
+	 * @return name of error page
+	 * */
+	@RequestMapping(value = "/500", method = RequestMethod.GET)
+	public String error500(Model model) {
+		model.addAttribute("title", "500");
+		model.addAttribute("head", "Проблеми сервера");
+		model.addAttribute("img", "/edsystem/resources/img/404.gif");
+		return "message";
+	}
+	
 }
---
+++ edsystem/src/main/webapp/static/js/views/message.jsp
@@ -0,0 +1,27 @@
+﻿<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@page session="false"%>
+<html>
+<head>
+  <title>${title}</title>
+
+  <!-- Bootstrap Core CSS -->
+  <link rel="stylesheet" href="<c:url value="/resources/css/bootstrap.min.css" />">
+
+</head>
+<body>
+<header id="top">
+  <div class="text-vertical-center">
+    <h1 style="text-align:center;"> ${head}</h1>
+    <br/>
+    <div style="text-align:center;">
+    	 <img src="${img}" alt="Train"/>
+    </div>
+    <br>
+    
+    <div id="main-button" style="text-align:center;">
+      <a href="<c:url value="/home"/>" class="btn btn-dark btn-lg" style="text-align:center;">Повернутися на головну сторінку</a>
+    </div>
+  </div>
+</header>
+</body>
+</html>
