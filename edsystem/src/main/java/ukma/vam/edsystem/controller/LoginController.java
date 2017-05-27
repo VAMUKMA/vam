a84171d1a3bff3531129c7aa50d36c3553276e68
Maksim <maksimmaksim1994@gmail.com>
2017-05-27 15:52:46 +0300

    authentification
    login

--- edsystem/src/main/java/ukma/vam/edsystem/controller/LoginController.java
+++ edsystem/src/main/java/ukma/vam/edsystem/controller/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -16,7 +17,17 @@ public class LoginController {
 	 * @return name of main page
 	 * */
 	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
-	public String welcomePage(Model model){
+	public String welcomePage(Model model, @RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "attention", required = false) String attention){
+		
+		if (error != null) {
+			model.addAttribute("msg", "Login/password is invalid");
+		}
+
+		if (attention != null) {
+			model.addAttribute("msg", "First log in, please");
+		}
+		
 		return "index";
 	}
 	
